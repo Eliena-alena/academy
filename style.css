a71ec7761637de1125e201b6052e49aaead7dc71
Eliena <elesbel@gmail.com>
2016-04-23 15:18:54 +0300

    first commit

---
+++ style.css
@@ -0,0 +1,66 @@
+div{
+    border: 2px solid black;
+    margin: 20px;
+
+}
+
+.wrapper{
+       width: 960px;
+       height: 100%; 
+       border: none;
+}
+
+.footer, .line1,.line2, .header{
+    height: 150px;
+}
+
+.main, .footer :after
+{
+    content: "";
+    display: block;
+    clear: both;
+}
+
+
+.main{
+    height: 408px;
+}
+
+.square{
+    height: 342px;
+    width: 27.7%;
+    float: left;
+    margin-right: 0;
+    background-color: green;
+}
+
+.line1,.line2{
+    width: 60%;
+    float: left;
+    background-color: #fff548;
+}
+
+.line1{
+    margin-bottom: 0px;
+}
+
+
+.block1, .block2 {
+    width: 50%;
+    height: inherit;
+    margin: 0;
+    float: left; 
+    border: none;
+     }
+
+.block1{
+    background-color: deepskyblue;
+  }
+
+.block2{
+    background-color: firebrick;
+}
+
+.line1,.line2, .header, .square{
+    padding: 10px;
+}
